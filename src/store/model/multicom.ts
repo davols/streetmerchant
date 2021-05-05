fee66a3f81472d15f0f77a1bc8a96673994b41f1
David Olsson <david@davido.me>
2021-05-05 20:30:47 +0200

    fix:broken multicom.no, added maxPrice

--- src/store/model/multicom.ts
+++ src/store/model/multicom.ts
@@ -4,12 +4,15 @@ export const Multicom: Store = {
   currency: 'kr.',
   labels: {
     inStock: {
-      container: '#ctl00_ContentMain_skPC_ctl00_ctl52_ctl02_btnBuy',
-      text: ['Legg i handlevogn'],
+      container:
+        '#ctl00_ContentMain_skPC_ctl00_ctl53_ctl02_btnBuy > div:nth-child(1) > span:nth-child(1)',
+      text: ['LEGG I HANDLEVOGN'],
+    },
+    maxPrice: {
+      container: '.b-product-price_',
     },
     outOfStock: {
-      container:
-        '#productCardContent > div.b-product-card > div.b-product-card__block-synopsys > div.b-price-block.b-price-block_visible > div.b-product-stock > div:nth-child(1) > a',
+      container: '.stock-notification-btn',
       text: ['MOTTA VARSEL!'],
     },
   },
@@ -64,140 +67,6 @@ export const Multicom: Store = {
       url:
         'https://www.multicom.no/business/gigabyte-geforce-rtx-3080-eagle/cat-p/c100134/p1001473457',
     },
-    {
-      brand: 'gigabyte',
-      model: 'gaming oc',
-      series: '3080',
-      url:
-        'https://www.multicom.no/business/gigabyte-geforce-rtx-3080-10gb/cat-p/c100134/p1001437239',
-    },
-    {
-      brand: 'msi',
-      model: 'suprim',
-      series: '3080',
-      url:
-        'https://www.multicom.no/business/msi-geforce-rtx-3080-suprim/cat-p/c100134/p1001547471',
-    },
-    {
-      brand: 'msi',
-      model: 'gaming',
-      series: '3080',
-      url:
-        'https://www.multicom.no/business/msi-geforce-rtx-3080-gaming/cat-p/c100134/p1001440885',
-    },
-    {
-      brand: 'inno3d',
-      model: 'ichill x3',
-      series: '3080',
-      url:
-        'https://www.multicom.no/business/inno3d-geforce-rtx-3080-10gb/cat-p/c100134/p1001439688',
-    },
-    {
-      brand: 'inno3d',
-      model: 'ichill frostbite',
-      series: '3080',
-      url:
-        'https://www.multicom.no/business/inno3d-geforce-rtx-3080-10gb/cat-p/c100134/p1001518239',
-    },
-    {
-      brand: 'inno3d',
-      model: 'ichill x4',
-      series: '3080',
-      url:
-        'https://www.multicom.no/business/inno3d-geforce-rtx-3080-10gb/cat-p/c100134/p1001439689',
-    },
-    {
-      brand: 'asus',
-      model: 'tuf',
-      series: '3080',
-      url:
-        'https://www.multicom.no/business/asus-tuf-rtx3080-10g-gaming-grafikkort-gf-rtx/cat-p/c100134/p1001439753',
-    },
-    {
-      brand: 'gigabyte',
-      model: 'aorus xtreme',
-      series: '3080',
-      url:
-        'https://www.multicom.no/business/gigabyte-aorus-geforce-rtx-3080/cat-p/c100134/p1001457889',
-    },
-    {
-      brand: 'pny',
-      model: 'xlr8 epic x',
-      series: '3080',
-      url:
-        'https://www.multicom.no/business/pny-xlr8-geforce-rtx-3080/cat-p/c100134/p1001440880',
-    },
-    {
-      brand: 'asus',
-      model: 'turbo',
-      series: '3080',
-      url:
-        'https://www.multicom.no/business/asus-turbo-rtx3080-10g-grafikkort-gf-rtx/cat-p/c100134/p1001603429',
-    },
-    {
-      brand: 'asus',
-      model: 'strix white',
-      series: '3080',
-      url:
-        'https://www.multicom.no/business/asus-rog-strix-rtx3080-o10g-white-oc-edition-grafikkort/cat-p/c100134/p1001547468',
-    },
-    {
-      brand: 'zotac',
-      model: 'amp holo',
-      series: '3080',
-      url:
-        'https://www.multicom.no/business/zotac-gaming-geforce-rtx-3080/cat-p/c100134/p1001440872',
-    },
-    {
-      brand: 'gigabyte',
-      model: 'vision oc',
-      series: '3080',
-      url:
-        'https://www.multicom.no/business/gigabyte-geforce-rtx-3080-vision/cat-p/c100134/p1001457887',
-    },
-    {
-      brand: 'gainward',
-      model: 'phoenix gs',
-      series: '3080',
-      url:
-        'https://www.multicom.no/business/gainward-geforce-rtx-3080-phoenix/cat-p/c100134/p1001441689',
-    },
-    {
-      brand: 'pny',
-      model: 'xlr8 gaming',
-      series: '3080',
-      url:
-        'https://www.multicom.no/business/gainward-geforce-rtx-3080-phoenix/cat-p/c100134/p1001441689',
-    },
-    {
-      brand: 'zotac',
-      model: 'trinity',
-      series: '3080',
-      url:
-        'https://www.multicom.no/business/zotac-gaming-geforce-rtx-3080/cat-p/c100134/p1001440871',
-    },
-    {
-      brand: 'asus',
-      model: 'strix white',
-      series: '3080',
-      url:
-        'https://www.multicom.no/business/asus-rog-strix-rtx3080-10g-white-grafikkort-gf-rtx/cat-p/c100134/p1001547470',
-    },
-    {
-      brand: 'zotac',
-      model: 'trinity oc',
-      series: '3080',
-      url:
-        'https://www.multicom.no/business/zotac-gaming-geforce-rtx-3080/cat-p/c100134/p1001440873',
-    },
-    {
-      brand: 'asus',
-      model: 'ekwb',
-      series: '3080',
-      url:
-        'https://www.multicom.no/business/asus-rtx3080-10g-ek-grafikkort-gf-rtx/cat-p/c100134/p1001518226',
-    },
-
     {
       brand: 'gigabyte',
       model: 'eagle',
